6e6467262eb1bc445b575610c151bb48275b2f78
Alexey Latyshev <latyshevskiymail@gmail.com>
2023-01-29 15:19:39 +0200

    finish counter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import './index.scss';
+import { useState } from "react";
+import "./index.scss";
 
 function App() {
+  let [value, setValue] = useState(0);
+
+  const onClickMinus = () => {
+    setValue((previousValue) => previousValue - 1);
+  };
+
+  const onClickPlus = () => {
+    setValue((previousValue) => previousValue + 1);
+  };
+
   return (
     <div className="App">
       <div>
         <h2>Счетчик:</h2>
-        <h1>0</h1>
-        <button className="minus">- Минус</button>
-        <button className="plus">Плюс +</button>
+        <h1>{value}</h1>
+        <button className="minus" onClick={onClickMinus}>
+          - Минус
+        </button>
+        <button className="plus" onClick={onClickPlus} type="button">
+          Плюс +
+        </button>
       </div>
     </div>
   );
