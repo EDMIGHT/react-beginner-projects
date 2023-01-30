e55abc627dde54d877897b4c060d76b350de7289
Alexey Latyshev <latyshevskiymail@gmail.com>
2023-01-30 14:29:57 +0200

    done modal window

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import './index.scss';
+import React, { useState } from "react";
+import "./index.scss";
+import Modal from "./Modal";
 
 function App() {
+  const [visible, setVisible] = useState(false);
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div> */}
+      <button className="open-modal-btn" onClick={() => setVisible(true)}>
+        ✨ Открыть окно
+      </button>
+      {/* если не нужна анимация, то такой способ нужно использовать */}
+      {/* {visible && <Modal setVisible={setVisible} />} */}
+      <Modal visible={visible} setVisible={setVisible}>
+        <img
+          src="https://i.pinimg.com/564x/f1/7c/53/f17c53323136e35087a420004c78d845.jpg"
+          alt="img"
+        />
+        <h3>Модальное окно</h3>
+      </Modal>
     </div>
   );
 }
